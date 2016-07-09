3337628acc61e4134aef1c3dc6cd07d27ea52910
German Utz <germanutz@gmail.com>
2016-07-09 20:03:49 -0300

    update definimos las relaciones del modelo Carrito

--- laravel/app/Carrito.php
+++ laravel/app/Carrito.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Carrito extends Model
 {
-    //
+    // Nombre tabla en MYSQL
+
+    protected $table = 'carritos';
+
+    // Atributos que se pueden asignar de manera masiva
+
+    protected $fillable = array('usuario_id');
+
+
+    // Relacion de Carrito con Usuario
+
+    public function usuarios(){
+
+        return $this->belongsTo('App\Usuario');
+
+    }
+    
+    public function productos(){
+        
+        return $this->hasMany('App\Producto');
+    }
+
 }
