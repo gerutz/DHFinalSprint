2b28747837b22b1c24219728612921f7a27802d7
German Utz <germanutz@gmail.com>
2016-07-09 20:11:35 -0300

    update implementamos modelo Comentario

--- laravel/app/Comentario.php
+++ laravel/app/Comentario.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    protected $table = 'comentarios';
+
+    protected $fillable = array('usuario_id');
+
+    // Definimos relaciones de las tablas
+
+    public function producto(){
+
+        return $this->belongsTo('App\Producto');
+
+    }
 }
