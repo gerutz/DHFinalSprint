6e5518a3295db260f5e07bafbc7e9341d30c4184
German Utz <germanutz@gmail.com>
2016-07-10 20:05:38 -0300

    apuntamos a una nueva base de datos dh_ecommerce_2

---
+++ laravel/database/migrations/2016_07_10_224731_create_carritos_table.php
@@ -0,0 +1,31 @@
+ph<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarritosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('carritos', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('carritos', function (Blueprint $table) {
+            //
+        });
+    }
+}
